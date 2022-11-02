5c007e9c6e31989f802ee602e91da90bf1a0b3ab
Sridhar117 <akula.suryasridhar@gmail.com>
2022-11-02 06:00:07 +0000

    initial commit

---
+++ src/components/BlogsList/index.js
@@ -0,0 +1,52 @@
+import {Component} from 'react'
+
+import {ColorRing} from 'react-loader-spinner'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogsList extends Component {
+  state = {blogsData: [], isLoading: true}
+
+  componentDidMount() {
+    this.blogData()
+  }
+
+  blogData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedData = data.map(eachItem => ({
+      author: eachItem.author,
+      avatarUrl: eachItem.avatar_url,
+      id: eachItem.id,
+      imageUrl: eachItem.image_url,
+      title: eachItem.title,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: updatedData, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <ColorRing
+            visible={isLoading}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogsList
